2b40a9a0f554a815e384db81987928fc798e2fdd
Orvin Demsy <orvindemsy@gmail.com>
2019-11-10 17:19:21 +0900

    first task creating UI for BCI motor imagery

---
+++ colors.py
@@ -0,0 +1,11 @@
+import pygame
+
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+blue = pygame.Color(0, 0, 255)
+red = pygame.Color(200, 0, 0)
+light_red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 170, 0)
+light_green = pygame.Color(0, 255, 0)
+orange = pygame.Color(255, 145, 0)
+gray = pygame.Color(128, 128, 128)
